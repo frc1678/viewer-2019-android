b279cf4db56c6c6412cb74c771fa646c566cb507
Teo Honda-Scully <32531217+thondascully@users.noreply.github.com>
2019-04-14 11:38:19 -0700

    Update seeding schema (#101)

--- androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/team_ranking/SeedingCell.java
+++ androidviewertemplates/src/main/java/com/example/evan/androidviewertemplates/team_ranking/SeedingCell.java
@@ -60,11 +60,10 @@ public class SeedingCell extends RelativeLayout {
         if (teamNumber != null) {
             Team teamTeam = FirebaseLists.teamsList.getFirebaseObjectByKey(teamNumber.toString());
 
-            String predictedSeed = (Utils.fieldIsNotNull(teamTeam, "calculatedData.predictedSeed") ? Utils.roundDataPoint(Utils.getObjectField(teamTeam, "calculatedData.predictedSeed"), 2, "???") : "???");
-            String predictedRankingPoints = (Utils.fieldIsNotNull(teamTeam, "calculatedData.predictedTotalNumRPs") ? Utils.roundDataPoint(Utils.getObjectField(teamTeam, "calculatedData.predictedTotalNumRPs"), 2, "???") : "???");
-            String currentRP = (Utils.fieldIsNotNull(teamTeam, "calculatedData.actualNumRPs") ? Utils.roundDataPoint(Utils.getObjectField(teamTeam, "calculatedData.actualNumRPs"), 2, "???") : "???");
-            String currentSeed = (Utils.fieldIsNotNull(teamTeam, "actualSeed") ? Utils.roundDataPoint(Utils.getObjectField(teamTeam, "actualSeed"), 2, "???") : "???");
-
+            String predictedSeed = (Utils.fieldIsNotNull(teamTeam, "calculatedData.predictedSeed") ? Utils.roundDataPoint(Utils.getObjectField(teamTeam, "calculatedData.predictedSeed"), 2, "?") : "?");
+            String predictedRankingPoints = (Utils.fieldIsNotNull(teamTeam, "calculatedData.predictedRPs") ? Utils.roundDataPoint(Utils.getObjectField(teamTeam, "calculatedData.predictedRPs"), 1, "?") : "?");
+            String currentRP = (Utils.fieldIsNotNull(teamTeam, "actualRPs") ? Utils.roundDataPoint(Utils.getObjectField(teamTeam, "actualRPs"), 2, "?") : "?");
+            String currentSeed = (Utils.fieldIsNotNull(teamTeam, "actualSeed") ? Utils.roundDataPoint(Utils.getObjectField(teamTeam, "actualSeed"), 2, "?") : "?");
             rankingTextView.setText(currentSeed);
             team.setText(teamNumber.toString());
             predictedSeedTV.setText(predictedSeed);
